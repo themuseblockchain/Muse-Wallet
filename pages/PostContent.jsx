82ef7ee61f34553fee939fb629c57b20f5db0cdf
Tadasu85 <johnstor@gmail.com>
2017-11-14 21:26:39 -0500

    A little more work on the fire function and integration.

--- pages/PostContent.jsx
+++ pages/PostContent.jsx
@@ -20,6 +20,76 @@ class PostContent extends Component {
         }
     }
 
+    postContent() {
+
+        let username = window.localStorage.getItem('username');
+
+        let password = window.localStorage.getItem('password');
+
+        muse.broadcast.content(
+
+                password,
+            ///
+                username,
+
+                $('#url').val(),
+                {
+          "part_of_album": false,///Boolean($('#part_of_album').val()),
+          "album_title": $('#album_title').val(),
+          "album_artist": [],
+          "genre_1": parseInt($('#album_genre_one').val()),
+          "country_of_origin": $('#country_of_origin').val(),
+          "explicit_": parseInt($('#explicit').val()),
+          "p_line": $('#album_p_line').val(),
+          "c_line": $('#album_c_line').val(),
+          "upc_or_ean": $('#upc_or_ean').val(),
+          "release_date": parseInt($('#release_date').val()),
+          "release_year": parseInt($('#release_year').val()),
+          "sales_start_date": parseInt($('#sales_start_date').val()),
+          "master_label_name": $('#master_label_name').val(),
+          "display_label_name": $('#display_label_name').val()
+        },
+        {
+          "track_title": $('#track_title').val(),
+          "ISRC": $('#isrc_code').val(),
+          "track_artists": [],
+          "genre_1": parseInt($('#track_genre_one').val()),
+          "p_line": $('#p_line').val(),
+          "track_no": parseInt($('#track_number').val()),
+          "track_volume": parseInt($('#track_volume').val()),
+          "track_duration": 1625362368,
+          "samples": false///Boolean($('#samples').val())
+        },
+        {
+          "composition_title": $('#composition_title').val(),
+          "third_party_publishers": false,///Boolean($('#third_party').val()),
+          "publishers": [],
+          "writers": [],
+          "PRO": ""
+        },
+              
+                [{
+                    "payee": username,
+                    "bp": 10000
+                  }
+                ],
+                [{
+                    "voter": username,
+                    "percentage": 100
+                  }
+                ],
+        100,
+    [],
+    [],
+        100,
+        10,
+        5000
+    ///
+    , function(err, result){console.log(err, result);
+
+  });
+    }
+
     render() {
         return (
             <div className="wallet-container margin-top-50 margin-bottom-65 tabs-wrapper">
@@ -59,12 +129,14 @@ class PostContent extends Component {
     
                 <input className="form-control" name="Copyright Number" id="copyright_number" placeholder="Copyright Number" />
 
-                <h1>Album Data:</h1>
+                <h1>Composition Data:</h1>
+
+                <input className="form-control" name="Composition Title" id="composition_title" placeholder="Composition Title" />
 
                 </div>
 
                 <div className="form-group margin-top-50">
-                    <button type="button" className="btn btn-primary" onClick={this.PostContent}>Post Content</button>
+                    <button type="button" className="btn btn-primary" onClick={this.postContent}>Post Content</button>
                 </div>                            
 
                 <div className={`loader-wrapper ${this.state.loading ? 'visible' : 'hidden'}`}></div>
